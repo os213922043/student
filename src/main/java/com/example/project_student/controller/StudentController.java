70b304499db84f91c0c24a4298ccd5948f991358
oybek_git <os213922043@gmail.com>
2025-01-31 13:11:29 +0500

    hello

---
+++ src/main/java/com/example/project_student/controller/StudentController.java
@@ -0,0 +1,41 @@
+package com.example.project_student.controller;
+
+import com.example.project_student.entity.Student;
+import com.example.project_student.service.StudentService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    // Barcha o'quvchilarni olish
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    // ID bo'yicha o'quvchini olish
+    @GetMapping("/{id}")
+    public Optional<Student> getStudentById(@PathVariable Long id) {
+        return studentService.getStudentById(id);
+    }
+
+    // Yangi o'quvchini ro'yxatdan o'tkazish
+    @PostMapping
+    public Student registerStudent(@RequestBody Student student) {
+        return studentService.registerStudent(student);
+    }
+
+    // O'quvchini ID bo'yicha o'chirish
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudent(id);
+    }
+}
---
+++ src/main/java/com/example/project_student/entity/Student.java
@@ -0,0 +1,27 @@
+package com.example.project_student.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "students")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
